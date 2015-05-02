5e876f0ec56f249b302ebbada831f533a26b99fe
Evandro Junior <evandrojr@gmail.com>
2015-05-02 14:48:02 -0300

    listing products nicely

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,8 @@ class Product < ActiveRecord::Base
       message: 'must be a URL for GIF, JPG or PNG image.'
   }
   
+  def self.latest
+    Product.order(:updated_at).last
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,7 @@ Product.create!(title: 'Programming Ruby 1.9 & 2.0',
   price: 49.95)
 # . . .
 
-Product.create!(title: 'Rails Test Prescriptions',
+Product.create!(title: 'Rails Test Prescriptions 2',
   description: 
     %{<p>
         <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
@@ -43,3 +43,39 @@ Product.create!(title: 'Rails Test Prescriptions',
       </p>},
   image_url: 'rtp.jpg',
   price: 34.95)
+  
+  Product.create!(title: 'CoffeeScript 2',
+    description: 
+      %{<p>
+          CoffeeScript is JavaScript done right. It provides all of JavaScript's
+  	functionality wrapped in a cleaner, more succinct syntax. In the first
+  	book on this exciting new language, CoffeeScript guru Trevor Burnham
+  	shows you how to hold onto all the power and flexibility of JavaScript
+  	while writing clearer, cleaner, and safer code.
+        </p>},
+    image_url:   'cs.jpg',    
+    price: 36.00)
+  # . . .
+  Product.create!(title: 'Programming Ruby 1.9 & 2.0 2nd edition',
+    description:
+      %{<p>
+          Ruby is the fastest growing and most exciting dynamic language
+          out there. If you need to get working programs delivered fast,
+          you should add Ruby to your toolbox.
+        </p>},
+    image_url: 'ruby.jpg',
+    price: 49.95)
+  # . . .
+
+  Product.create!(title: 'Rails Test Prescriptions 4',
+    description: 
+      %{<p>
+          <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
+          Rails applications, covering Test-Driven Development from both a
+          theoretical perspective (why to test) and from a practical perspective
+          (how to test effectively). It covers the core Rails testing tools and
+          procedures for Rails 2 and Rails 3, and introduces popular add-ons,
+          including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
+        </p>},
+    image_url: 'rtp.jpg',
+    price: 34.95)
